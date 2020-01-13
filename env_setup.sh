8e304411db7a7a1a2c7f388415365bb5d833c7c6
sam <samuele.germiniani@univr.it>
2020-01-13 12:10:06 +0100

    Hotfix antrl4 libs path

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/mining/a-team/libs/lib/spot/lib/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/mining/a-team/libs/lib/antrl4/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/mining/a-team/libs/lib/antlr4/lib/
 export PATH=$PATH:~/modeltech/bin
