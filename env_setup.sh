91faa01238bfdb4fc292b5c452fd3ac2b17a8a5e
sam <samuele.germiniani@univr.it>
2020-01-13 13:41:23 +0100

    Added path to bin and license

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,4 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/mining/a-team/libs/lib/spot/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/mining/a-team/libs/lib/antlr4/lib/
-export PATH=$PATH:~/modeltech/bin
+export PATH=$PATH:/usr/local/Questa-2019.4/questasim/bin/
+export MGLS_LICENSE_FILE=1717@157.27.6.46
